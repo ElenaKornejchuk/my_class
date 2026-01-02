4260d3f06a8fac9125758f500e4b93a30bd27d5b
ElenaKorneichuk <t.9857294674@gmail.com>
2026-01-02 12:14:30 +0300

    lesson8

---
+++ pythonProject/lesson8.py
@@ -0,0 +1,49 @@
+
+# a = 1
+# print(id(a))
+#
+# a += 1
+# print(a)
+# print(id(a))
+#
+# a = 1
+# b = a
+# b += 1
+#
+# print(id(a))
+# print(b)
+# print(id(b))
+
+# fruits = ["яблоко", "груша", "апельсин"]
+#
+# fruits2 = fruits
+#
+# fruits2.append("слива")
+#
+# print(fruits)
+# print(fruits2)
+# print(id(fruits))
+# print(id(fruits2))
+
+#
+# fruits = ["яблоко", "груша", "апельсин"]
+# #
+# # fruits2 = fruits.copy()
+# # fruits2.append("слива")
+# #
+# # print(fruits)
+# # print(fruits2)
+# # print(id(fruits))
+# # print(id(fruits2))
+#
+# fruits2 = list(fruits)
+# print(fruits)
+# print(fruits2)
+# print(id(fruits))
+# print(id(fruits2))
+
+
+
+
+
+
