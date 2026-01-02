e53d3ad0a3271796e657e28141668094ca09ecc0
ElenaKorneichuk <t.9857294674@gmail.com>
2026-01-02 12:12:17 +0300

    lesson3

---
+++ pythonProject/lesson3.py
@@ -0,0 +1,93 @@
+# a = 100
+# b = 0
+# c = -100
+
+# int — целые числа
+# print(type(c))
+
+#
+#
+# a = 100.456345
+# b = 0.454565454
+# c = -10.2
+#
+# # float  число с плавающей точкой или вещественные
+# print(type(c))
+#
+# sum = 0.1 + 0.2
+# print(sum)
+#
+#
+# pi =3.14159
+# # print(round(pi))
+# print(round(pi, 3))
+
+# a = 2.5
+# b = 3.5
+# print(round(a))
+# print(round(b))
+
+# print(0.1 + 0.2)
+#
+# from decimal import Decimal
+#
+# a = Decimal("0.1")
+# b = Decimal("0.2")
+# print(a + b)
+
+# bool — это True или False.
+
+# a, b = 10, 3
+#
+# print(a > b)   # True  — больше
+# print(a < b)   # False — меньше
+# print(a >= b)  # больше или равно
+# print(a <= b)  # меньше или равно
+# print(a == b)  # равно
+# print(a != b)  # не равно
+
+#
+# nothing = None
+# print(nothing)
+#
+# print(type(nothing))
+# print(None == 0)
+# print(None == "")
+# print(None is None)
+#
+#
+# lst = [1, 2, 3, 4]                # list — список (изменяемый, итерируемый)
+# tup = (1, 2, 3, 4)                # tuple — кортеж (неизменяемый, итерируемый)
+# st  = {1, 2, 3, 4}                # set — множество (уникальные элементы)
+# dct = {"первый": 1, "второй": 2}  # dict — словарь (ключ–значение)
+
+# a = 10
+# b = 3
+#
+# # print(a + b)   # сложение
+# # print(a - b)   # вычитание
+# # print(a * b)   # умножение
+# # print(a / b)   # деление (результат float)
+# # print(a // b)  # целая часть от деления (floor)
+# # print(a % b)   # остаток от деления
+# # print(a ** b)  # возведение в степень
+#
+#
+# print(a + b * 2)
+# print((a + b) * 2)
+#
+# x = 5
+# x = x + 2
+# x += 2
+# print(x)
+
+#
+# text = "10"
+# number = 5
+# result = int(text) + number
+# print(result)
+
+
+
+
+
