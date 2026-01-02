a4b26e28cb731b89a69b858caf8e7f0bd88de319
ElenaKorneichuk <t.9857294674@gmail.com>
2026-01-02 12:14:37 +0300

    lesson9

---
+++ pythonProject/lesson9.py
@@ -0,0 +1,71 @@
+# # # fruits = ("apple", "banana", "cherry")
+# # # print(type(fruits))
+# # #
+# # # colors = ("red", "green", "blue", "yellow")
+# # # print(colors[1])
+# # from pythonProject.lesson3 import number
+# #
+# # # cars = ("BMW", "Audi", "Tesla")
+# # # cars[0] = "Mercedes"
+# # # print(cars)
+# #
+# # # data = ("John", 25, ["Python", "Java"])
+# # # data[2].append("C++")
+# # # print(data)
+# from pythonProject.lesson3 import number
+#
+# #
+# animals = ("cat", "dog", "parrot")
+# lst = list(animals)
+# lst.append("hamster")
+# lst.remove("dog")
+# animals = tuple(lst)
+# print(animals)
+#
+# letters = ("a", "b", "a", "c", "a")
+# print(letters.count("a"))
+
+#
+# items = ("hello", 123, 45.6, [1, 2, 3])
+#
+# number = []
+#
+# for i in items:
+#     if isinstance(i, str):
+#         number.append(i)
+#
+# print(number)
+
+
+# scores = (50, 20, 70, 10, 40)
+# sorted_scores = sorted(scores)
+# sorted_scores2 = tuple(sorted_scores)
+#
+# print(sorted_scores)
+# print(sorted_scores2)
+# print(type(sorted_scores))
+# print(type(sorted_scores2))
+
+
+days = ("Mon", "Tue", "Wed")
+days_list = list(days)
+days_list.append("Thu")
+days = tuple(days_list)
+print(days)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
