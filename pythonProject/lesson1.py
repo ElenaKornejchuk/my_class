fca3b6f9db61d756ee8d92effabf82c0f32ef15a
ElenaKorneichuk <t.9857294674@gmail.com>
2026-01-02 12:12:06 +0300

    lesson1

---
+++ pythonProject/lesson1.py
@@ -0,0 +1,23 @@
+# print("Hello world!")
+
+# print("Я учу", "Python", "с нуля") # комментари
+#
+# print("01", "01", "2025")
+
+# print("Привет")
+# print("Python")
+#
+# print("Привет", end="/")
+# print("Python")
+
+# name = "Elena"
+# print(name)
+#
+# # print(age)
+# user_name = "Ivan"
+
+# age = 25
+# age = 30
+# age = 25
+#
+# print(age)
