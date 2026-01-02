53ca5ef8c6fe97df9f87cbf7d2937084626f4a56
ElenaKorneichuk <t.9857294674@gmail.com>
2026-01-02 12:14:52 +0300

    lesson11

---
+++ pythonProject/lesson11.py
@@ -0,0 +1,36 @@
+#
+# numbers = {1, 2, 3, 3, 4, 5}
+# print(numbers)
+#
+# unique_letters = set("banana")
+# print(unique_letters)
+
+'''
+.add(element) добавить элемент
+.discard(element) удалить элемент, но если его нет то не вызывает ошибку
+.union(other) объединяет несколько множеств
+.intersection(other) пересечение множеств
+.difference(other) множества элементов не принадлежащие ни к одному other'''
+from enum import unique
+import random
+
+
+# number = {1, 2, 3}
+# number.add(4)
+# print(number)
+#
+# number.discard(2)
+# print(number)
+
+# set1 = {1, 2, 3, 4, 5}
+# set2 = {4, 5, 6, 7, 8}
+#
+# print(set1.union(set2))
+# print(set1.intersection(set2))
+# print(set2.difference(set1))
+
+# data = [1, 2, 3, 3, 3, 4, 5, 5, 6]
+# unique_data = list(set(data))
+#
+
+
