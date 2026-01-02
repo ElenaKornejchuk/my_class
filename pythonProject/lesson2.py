71131e4dde81d5ba440892d7dfb49f2390a39131
ElenaKorneichuk <t.9857294674@gmail.com>
2026-01-02 12:14:05 +0300

    lesson2

---
+++ pythonProject/lesson2.py
@@ -0,0 +1,101 @@
+
+# скалярные (простые, неитерируемые):
+# int – целое число
+# float – число с плавающей точкой
+# bool – логический тип
+# NoneType – специальное "ничего"
+
+# последовательности и коллекции (итерируемые):
+# str  – строка (текст)
+# list – список
+# tuple – кортеж
+# set – множество (уникальные элементы)
+# dict – словарь (пары ключ–значение)
+
+# print(type("Привет"))
+# print(type(10))
+# print(type(10.5))
+# print(type(True))
+# print(type(None))
+
+# text1 = "Hello"
+# text2 = 'И это тоже строка'
+# print(type(text1), type(text2))
+#
+# text1 = "I'm learning Python"
+# text2 = 'I\'m learning Python'
+# print(text2)
+
+# bio = """Меня зовут
+# ;;;;
+# ''''
+# hhh
+# Татьяна"""
+# print(bio)
+#
+# """Многострочный
+# комментарий"""
+
+# name = "Иван"
+# surname = "Иванов"
+# # print(name + " " + surname)
+#
+# print(f"Моё имя {name}, фамилия {surname}")  # f-строка (удобнее и понятнее)
+#
+# print("Моё имя {}, фамилия {}".format(name, surname))  # старый способ
+
+# txt = "Привет!"
+# # print(txt * 5)
+#
+# print(len(txt))
+#
+# print("б" in txt)
+
+# text = "Привет!"
+# print(text[0])
+# print(text[-1])
+# print(text[:3])
+# print(text[1:4])
+# print(text[3:])
+# print(text[::-1])
+# print(text[2::-1])
+
+# full = "иванов Иван иванович"
+# print(full.lower())
+# print(full.upper())
+# print(full.title())
+# print(full.capitalize())
+#
+# print("123".isdigit())
+# print("HELLO".isupper())
+#
+# print("banana".count("a"))
+# print("банан".replace("а", "A"))
+
+# num = "+7"
+# print(num.ljust(12, "0"))
+# print(num.center(12, "-"))
+
+#
+# text = "  Привет  "
+# print(text.strip())
+#
+# text = "my\name"
+# print(text)
+
+# \n \t \v и др.
+#
+# text = "my\\name"
+# print(text)
+#
+# raw  = r"my\name"
+# print(raw)
+
+
+
+
+
+
+
+
+
